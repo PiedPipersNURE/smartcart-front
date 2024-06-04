57a0080d3bbeb68ac89e40fe99ceda95e7164336
PetrPoling <daniilberegovoj62@gmail.com>
2024-06-04 11:36:53 +0200

    reniew review

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,23 @@
-import './Header.css'
-import logo from "../Assets/logo.png"
-function Header(){
-    return <div className = "header">
-        <img src = {logo} className = "header-logo"></img>
-        <ul className = "right-buttons">
-        <li><button /*onClick = {}*/ className = "right-button">Contacts</button></li>
-        <li><button /*onClick = {}*/className = "right-button">Rewiews</button></li>
-        <li><button /*onClick = {}*/className = "right-button" id = "sign-in">Sign in</button></li>
-        </ul>
-    </div>
+import React from 'react';
+import './Header.css';
+import logo from "../Assets/logo.png";
+
+function Header() {
+    const scrollToReviews = () => {
+        // Scroll down by 5500 pixels when "Reviews" button is clicked
+        window.scrollBy(0, 5500);
+    };
+
+    return (
+        <div className="header">
+            <img src={logo} className="header-logo" alt="Logo"></img>
+            <ul className="right-buttons">
+                <li><button className="right-button">Contacts</button></li>
+                <li><button className="right-button" onClick={scrollToReviews}>Reviews</button></li>
+                <li><button className="right-button" id="sign-in">Sign in</button></li>
+            </ul>
+        </div>
+    );
 }
 
-export default Header
+export default Header;
